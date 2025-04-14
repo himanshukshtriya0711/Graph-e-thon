0791d546a4feeb33709f3351f1ffcce94b4e4e59
himanshu <himanshu.singh_cs23@gla.ac.in>
2025-04-14 20:51:06 +0530

    made the ui chnage and added a toggle button

--- tumor_detection/apps.py
+++ tumor_detection/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TumorDetectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tumor_detection'
+    
+    def ready(self):
+        import tumor_detection.templatetags.form_tags
